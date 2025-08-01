024b004750f2a4d9eddc50fde80e674db28f0e9a
Piyush88900 <piyushsah396@gmail.com>
2025-08-01 18:31:41 +0530

    Create auth.module.ts

---
+++ Auth Module/src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({ secret: 'secretKey123', signOptions: { expiresIn: '1h' } }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
